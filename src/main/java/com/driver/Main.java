fe58052b2654a523d86d071f0e4db2dd7a458904
niteshsoni7699 <niteshsoni7699@gmail.com>
2024-02-21 13:25:39 +0530

    initial commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args){
+
+        RWOnly obj = new RWOnly();
+        obj.name = "hello";
+        obj.setName("Abhishek");
+        String ans = obj.getName();
+
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -2,4 +2,17 @@ package com.driver;
 
 public class RWOnly {
     
+    private String name = 10;
+
+    public RWOnly(String name){
+        this.name = name;
+    }
+
+    void setName(String name){
+        this.name = name;
+    }
+
+    String getName(){
+        return this.name;
+    }
 }
