de434fbd09487ad8185c7331a30b7a2fbd101cf8
niteshsoni7699 <niteshsoni7699@gmail.com>
2024-02-21 13:35:22 +0530

    initial commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,8 +3,9 @@ package com.driver;
 public class Main {
     public static void main(String[] args){
 
-        RWOnly obj = new RWOnly("pandey");
+        RWOnly obj = new RWOnly("helloww");
         obj.name = "hello";
+        System.out.println(obj.name); //name has private access in com.driver.RWOnly
         obj.setName("Abhishek");
         String ans = obj.getName();
 
